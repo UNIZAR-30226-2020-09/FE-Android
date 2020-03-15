c89938715d2e14536a73bb2d2b1965e9ea768e7d
irefu <758325@unizar.es>
2020-03-15 19:15:22 +0100

    Test 19:14

---
+++ app/src/main/java/es/unizar/eina/pandora/Registro.java
@@ -0,0 +1,14 @@
+package es.unizar.eina.pandora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+    //Prueba 19:14
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
