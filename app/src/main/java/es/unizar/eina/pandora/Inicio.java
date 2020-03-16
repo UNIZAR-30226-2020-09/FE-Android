5dd630b7b708072d10aade9a40a45408b31458fd
irefu <758325@unizar.es>
2020-03-16 13:08:24 +0100

    "Pantalla Inicio"

---
+++ app/src/main/java/es/unizar/eina/pandora/Inicio.java
@@ -0,0 +1,36 @@
+package es.unizar.eina.pandora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+    private Button login;
+    private Button registro;
+    private Button contacto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        login = findViewById(R.id.inicio_login);
+        registro = findViewById(R.id.inicio_registro);
+        contacto = findViewById(R.id.inicio_contacto);
+    }
+
+    public void goLogin(View view){
+        startActivity(new Intent(Inicio.this,Login.class));
+    }
+
+    public void goRegistro(View view){
+        startActivity(new Intent(Inicio.this, Registro.class));
+    }
+
+    public void goContacto(View view){
+        startActivity(new Intent(Inicio.this, Contacto.class));
+    }
+}
