24af68262d0e25aee372c1e5978f885f16126ddf
irefu <758325@unizar.es>
2020-03-16 17:13:20 +0100

    "Registro primera parte"

--- app/src/main/java/es/unizar/eina/pandora/Registro.java
+++ app/src/main/java/es/unizar/eina/pandora/Registro.java
@@ -1,14 +1,109 @@
 package es.unizar.eina.pandora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
-    //Prueba 19:14
+
+
+    private Button siguiente;
+
+    private EditText email;        //Campo para escribir el email
+
+    private Boolean emptyEmail = true;      //Campo del email vacío
+
+    private Boolean emailCheckLength = false;
+
+    private Boolean emailCheckValue = false;
+
+    private TextView limitEmail;
+
+    private String emailIntroducido;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        siguiente=findViewById(R.id.registro_siguiente);
+        email = findViewById(R.id.registro_correo);
+
+
+        limitEmail = findViewById(R.id.registro_long_correo);
+
+        email.addTextChangedListener(registerTextWatcher);
+
+
+    }
+
+    private TextWatcher registerTextWatcher = new TextWatcher() {
+
+        private String correo;
+        private String newLimitEmail;
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            correo= email.getText().toString().trim();
+
+            Integer mailchars = 100 - correo.length();
+            if (mailchars < 0) {
+                limitEmail.setTextColor(Color.rgb(200,0,0));
+            }
+            else {
+                limitEmail.setTextColor(Color.rgb(128,128,128));
+            }
+            newLimitEmail = mailchars.toString();
+
+            limitEmail.setText(newLimitEmail);
+
+            emptyEmail = correo.isEmpty();
+        }
+
+        @Override // Soloo puede tener letras mayusculas, minusci,as si n acentuar numeros y _-.
+        public void afterTextChanged(Editable s) {
+            Log.d("MyApp","++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Log.d("MyApp", Integer.toString(correo.length()));
+            emailCheckLength = (correo.length() <= 100 && correo.length() >= 3);
+            emailCheckValue = correo.matches("[a-zA-Z0-9_.]+@[a-zA-Z0-9_.]+");
+
+            siguiente.setEnabled(!emptyEmail);
+            if (siguiente.isEnabled()) {
+                siguiente.setBackgroundTintList(AppCompatResources.getColorStateList(siguiente.getContext(), R.color.colorButtonEnabled));
+            }
+            else {
+                siguiente.setBackgroundTintList(AppCompatResources.getColorStateList(siguiente.getContext(), R.color.colorButtonDisabled));
+            }
+        }
+    };
+
+    // Se registra el usuario si se cumplen todos los requisitos, si no se muestra el error.
+    public void goSiguiente(View view){
+        if (!emailCheckLength) {
+            Toast.makeText(getApplicationContext(),"La dirección de correo tiene que tener entre 3 y 100 caracteres.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            emailIntroducido = email.getText().toString().trim();
+
+            //Enviamos el email que ha introducido el usuario a la actividad RegistroDos
+            Intent aux = new Intent(this, RegistroDos.class);
+            aux.putExtra("email",emailIntroducido);
+            startActivity(aux);
+        }
     }
 }
